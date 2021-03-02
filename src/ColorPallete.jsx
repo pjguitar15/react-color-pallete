112cde7175d84a575692d238701c6f8eccac3222
Phil <pjguitar15@gmail.com>
2021-03-02 16:43:03 +0800

    test

--- src/ColorPallete.jsx
+++ src/ColorPallete.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { CopyToClipboard } from 'react-copy-to-clipboard'
 const ColorPallete = () => {
   const [bgColor, setBgColor] = useState('')
